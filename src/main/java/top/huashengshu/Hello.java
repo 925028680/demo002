1c2b855b07d2657e275c6e4383735c4ea74cb4a1
lijie <925028680@qq.com>
2020-05-08 22:33:59 +0800

    Revert "123456"
    This reverts commit fce61667e154d0b009e48dbb29d567583b640617.

--- src/main/java/top/huashengshu/Hello.java
+++ src/main/java/top/huashengshu/Hello.java
@@ -2,6 +2,6 @@ package top.huashengshu;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("123456");//改变后的
+        System.out.println("123456");//改变后的提交
     }
 }
